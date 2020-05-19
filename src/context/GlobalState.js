9a0b63d01c37383f5a7435dc18ce1864c8bfb274
Shola Ayeni <ayenisholah@yahoo.com>
2020-05-19 15:56:07 +0100

    created delete playlist item action

--- src/context/GlobalState.js
+++ src/context/GlobalState.js
@@ -33,3 +33,17 @@ const initialState = {
 // create context
 export const GlobalContext = createContext(initialState);
 
+// Provider component
+export const GlobalProvider = ({ children }) => {
+  const [state, dispatch] = useReducer(AppReducer, initialState);
+
+  //   Actions
+  function deletePlaylistItem(id) {
+    dispatch({
+      type: DELETE_PLAYLIST_ITEM,
+      payload: id
+    });
+  }
+
+ 
+};
